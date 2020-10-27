3f6d63c433920f0a3234e0c1a6bc0118be37ffec
aayusharya1 <aayusharya07@gmail.com>
2020-10-27 21:28:16 +0530

    aa

---
+++ TplProjects/CsvJsonManipulation.cs
@@ -0,0 +1,23 @@
+﻿using CsvHelper;
+using Newtonsoft.Json.Schema;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace TplProjects
+{
+    class CsvJsonManipulation
+    {
+        public static void ImplementJsonToCsv() 
+        {
+            string importFilePath = @"";
+            string exportFilePath = @"";
+            IList<AddressData> addressData = JsonConvert.DeserializeObject<IList<AddressData>>[FileStyleUriParser.ReadAllText(importFilePath)];
+            Console.WriteLine("*************Now reading from jsonfile and write to csv file********");
+            // writing csv file
+            using (var writer = new StreamWriter(exportFilePath))
+            using (ValidationEventArgs csvExport =new CsvWriter())
+        }
+    }
+}
